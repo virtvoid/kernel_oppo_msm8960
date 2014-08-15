00b16b66e91aecdaa514644949df8ee9fc4360c1
virtvoid <boss@fake.net>
2014-08-15 19:55:36 +0200

    ugh, forgot to add 384 MHz GPU to table...

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -3528,6 +3528,7 @@ static struct clk_freq_tbl clk_tbl_gfx3d[] = {
 	F_GFX3D(228571000, pll2,  2,  7),
 	F_GFX3D(266667000, pll2,  1,  3),
 	F_GFX3D(320000000, pll2,  2,  5),
+	F_GFX3D(384000000, pll2,  2,  5),
 	F_GFX3D(400000000, pll2,  1,  2),
 	F_GFX3D(450000000, pll15, 1,  2),
   F_GFX3D(457142000, pll15, 1,  2),
