6ba633b09427acd7033e2232e2c4ff213a86776c
virtvoid <boss@fake.net>
2014-09-19 22:19:03 +0200

    switch to Intelli-Thermal

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -19,19 +19,13 @@
 struct msm_thermal_data {
 	uint32_t sensor_id;
 	uint32_t poll_ms;
-	uint32_t shutdown_temp;
-
-	uint32_t allowed_max_high;
-	uint32_t allowed_max_low;
-	uint32_t allowed_max_freq;
-
-	uint32_t allowed_mid_high;
-	uint32_t allowed_mid_low;
-	uint32_t allowed_mid_freq;
-
-	uint32_t allowed_low_high;
-	uint32_t allowed_low_low;
-	uint32_t allowed_low_freq;
+	int32_t limit_temp_degC;
+	int32_t temp_hysteresis_degC;
+	uint32_t freq_step;
+	uint32_t freq_control_mask;
+	int32_t core_limit_temp_degC;
+	int32_t core_temp_hysteresis_degC;
+	uint32_t core_control_mask;
 };
 
 struct msm_thermal_stat {
