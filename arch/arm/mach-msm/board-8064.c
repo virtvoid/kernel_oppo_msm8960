f887fa4f1e8ab15f05fb4f6acca84a6a0e8a5405
virtvoid <boss@fake.net>
2014-09-29 21:12:00 +0200

    msm_void_thermal management v1
    Conflicts:
    	arch/arm/mach-msm/board-8064.c
    	drivers/thermal/msm_thermal.c
    	include/linux/msm_thermal.h

--- arch/arm/mach-msm/board-8064.c
+++ arch/arm/mach-msm/board-8064.c
@@ -2345,20 +2345,14 @@ static struct platform_device msm_tsens_device = {
 
 static struct msm_thermal_data msm_thermal_pdata = {
 	.sensor_id = 7,
-	.poll_ms = 150,
-	.shutdown_temp = 88,
-
-	.allowed_max_high = 84,
-	.allowed_max_low = 80,
-	.allowed_max_freq = 384000,
-
-	.allowed_mid_high = 81,
-	.allowed_mid_low = 76,
-	.allowed_mid_freq = 810000,
-
-	.allowed_low_high = 79,
-	.allowed_low_low = 73,
-	.allowed_low_freq = 1350000,
+	.poll_ms = 300,
+	.limit_overtemp_degC = 88,
+	.limit_temp_degC = 81,
+	.core_limit_temp_degC = 82,
+	.temp_hysteresis_degC = 10,
+	.freq_control_mask = 0xb,
+	.core_temp_hysteresis_degC = 8,
+	.core_control_mask = 0x4,
 };
 
 #define MSM_SHARED_RAM_PHYS 0x80000000
--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -19,19 +19,14 @@
 struct msm_thermal_data {
 	uint32_t sensor_id;
 	uint32_t poll_ms;
-	uint32_t shutdown_temp;
-
-	uint32_t allowed_max_high;
-	uint32_t allowed_max_low;
-	uint32_t allowed_max_freq;
-
-	uint32_t allowed_mid_high;
-	uint32_t allowed_mid_low;
-	uint32_t allowed_mid_freq;
-
-	uint32_t allowed_low_high;
-	uint32_t allowed_low_low;
-	uint32_t allowed_low_freq;
+	int32_t limit_overtemp_degC;
+	int32_t limit_temp_degC;
+	int32_t temp_hysteresis_degC;
+	uint32_t freq_step;
+	uint32_t freq_control_mask;
+	int32_t core_limit_temp_degC;
+	int32_t core_temp_hysteresis_degC;
+	uint32_t core_control_mask;
 };
 
 struct msm_thermal_stat {
@@ -46,6 +41,7 @@ struct msm_thermal_stat {
 #ifdef CONFIG_THERMAL_MONITOR
 extern int msm_thermal_init(struct msm_thermal_data *pdata);
 extern int msm_thermal_device_init(void);
+extern int msm_thermal_update_limits(void);
 #else
 static inline int msm_thermal_init(struct msm_thermal_data *pdata)
 {
